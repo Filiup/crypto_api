6b7301e74386e33cf0625dfbffb66ee8e403f250
Filip Barabas <filip.barabas@student.tuke.sk>
2025-03-30 18:04:43 +0200

    Re-generate migration

--- crypto/models/cryto_currency_model.py
+++ crypto/models/cryto_currency_model.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
 from db.database import Base
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import ARRAY
 
 @dataclass
 class CryptoCurrencyModel(Base):
     __tablename__ = "crypto_currency"
     id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
-    symbol: Mapped[str] = mapped_column(unique=True)
     coingecko_id: Mapped[str] = mapped_column(unique=True)
+    categories: Mapped[list[str]] = mapped_column(ARRAY(String), server_default="{}")
+    symbol: Mapped[str]
     hashing_algorithm: Mapped[str]
-    categories: Mapped[list[str]]
     name: Mapped[str]
 
     def __repr__(self):
