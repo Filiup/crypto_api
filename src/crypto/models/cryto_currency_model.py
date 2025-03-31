351eb5d4db427913cbf6630924e854e5beae227d
Filip Barabas <filip.barabas@student.tuke.sk>
2025-03-31 21:17:05 +0200

    Add table indexes

--- src/crypto/models/cryto_currency_model.py
+++ src/crypto/models/cryto_currency_model.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
 import datetime
 from db.database import Base
-from sqlalchemy import String, DateTime, BigInteger
+from sqlalchemy import String, DateTime, BigInteger, Index
+
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.sql import func
@@ -9,8 +10,9 @@ from sqlalchemy.sql import func
 @dataclass
 class CryptoCurrencyModel(Base):
     __tablename__ = "crypto_currency"
+
     id: Mapped[int] = mapped_column(primary_key=True, index=True, unique=True)
-    coingecko_id: Mapped[str] = mapped_column(unique=True)
+    coingecko_id: Mapped[str] = mapped_column(unique=True, index=True)
     categories: Mapped[list[str]] = mapped_column(ARRAY(String), server_default="{}")
 
     current_price: Mapped[int] = mapped_column(BigInteger, nullable=True)
@@ -19,9 +21,13 @@ class CryptoCurrencyModel(Base):
     last_updated: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     image_url: Mapped[str] = mapped_column(nullable=True)
 
-    symbol: Mapped[str]
+    symbol: Mapped[str] = mapped_column(index=True)
     hashing_algorithm: Mapped[str]
-    name: Mapped[str]
+    name: Mapped[str] = mapped_column(index=True)
+
+    __table_args__ = (
+        Index("idx_crypto_currency_categories_gin", categories, postgresql_using='gin'),
+    )
 
     def __repr__(self):
         return f"CryptoCurrencyEntity(id={self.id}, symbol={self.symbol}, coingecko_id={self.coingecko_id}, name={self.name})"
